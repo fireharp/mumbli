8d8057e86d2edbb347f9de3124497dddb604cc28
Alex Yanitskiy <demo.alex@gmail.com>
2026-05-09 21:49:48 +0200

    docs: adopt editorial landing page

--- docs/ahrefs-analytics.js
+++ docs/ahrefs-analytics.js
@@ -1,5 +1,7 @@
-const script = document.createElement('script');
-script.src = 'https://analytics.ahrefs.com/analytics.js';
-script.setAttribute('data-key', 'J0o8ZmiFywSrbPacaXtgbQ');
-script.async = true;
-document.head.appendChild(script);
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://analytics.ahrefs.com/analytics.js";
+  script.setAttribute("data-key", "J0o8ZmiFywSrbPacaXtgbQ");
+  script.async = true;
+  document.head.appendChild(script);
+})();
--- docs/umami-analytics.js
+++ docs/umami-analytics.js
@@ -1,5 +1,7 @@
-const script = document.createElement('script');
-script.src = 'https://cloud.umami.is/script.js';
-script.setAttribute('data-website-id', '545e61a4-c215-461d-a9d8-5c567e798617');
-script.defer = true;
-document.head.appendChild(script);
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://cloud.umami.is/script.js";
+  script.setAttribute("data-website-id", "545e61a4-c215-461d-a9d8-5c567e798617");
+  script.defer = true;
+  document.head.appendChild(script);
+})();
